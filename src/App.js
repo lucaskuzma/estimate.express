a299386092d9d72c88816ae41904696868415b3f
lucas <lucas@thestrangeagency.com>
2018-07-21 15:11:02 +0200

    move utils into their own file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-
-// https://developer.mozilla.org/en-US/docs/Web/API/WindowBase64/Base64_encoding_and_decoding
-
-function b64EncodeUnicode(str) {
-    // first we use encodeURIComponent to get percent-encoded UTF-8,
-    // then we convert the percent encodings into raw bytes which
-    // can be fed into btoa.
-    return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g,
-        function toSolidBytes(match, p1) {
-            return String.fromCharCode('0x' + p1);
-    }));
-}
-
-function b64DecodeUnicode(str) {
-    // Going backwards: from bytestream, to percent-encoding, to original string.
-    return decodeURIComponent(atob(str).split('').map(function(c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-    }).join(''));
-}
+import {b64EncodeUnicode} from './utils';
+import {b64DecodeUnicode} from './utils';
 
 class App extends Component {
   constructor(props) {
