4cff7b8dc620e7e02e8cce3da2205c46d606b7a0
lucas <lucas@thestrangeagency.com>
2018-07-21 09:56:50 +0200

    getting and writing text

--- src/App.js
+++ src/App.js
@@ -1,18 +1,61 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 
+      `500/d
+2300/wk
+
+install react 1d
+set up repo 1h
+hack css 1w
+  
+      `,
+      output: [],
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  componentDidMount() {
+    this.updateResult(this.state.value)
+  }
+
+  handleChange(event) {
+    this.updateResult(event.target.value);
+  }
+
+  updateResult(text) {
+    var output = []
+    var lines = text.split('\n');
+    for(var i = 0;i < lines.length;i++) {
+      output.push(lines[i]+'\n');
+
+    }
+
+    this.setState(
+      {
+        value: text,
+        output: output,
+      }
+    );
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <code>
+          {this.state.output}
+        </code>
+        <form>
+          <textarea className="App-entryArea" rows="20" type="text" value={this.state.value} onChange={this.handleChange} />
+      </form>
       </div>
     );
   }
